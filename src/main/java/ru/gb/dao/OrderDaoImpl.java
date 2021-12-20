5ee2d41246309bf269abf7e26e02f79d04bd361c
KuznetsovM <kuznetcoff@mail.ru>
2021-12-20 17:13:10 +0300

    init

--- src/main/java/ru/gb/dao/OrderDaoImpl.java
+++ src/main/java/ru/gb/dao/OrderDaoImpl.java
@@ -1,34 +1,17 @@
 package ru.gb.dao;
 
-import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 import ru.gb.entity.Order;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.persistence.TypedQuery;
-
-@Repository
-@Transactional
 public class OrderDaoImpl implements OrderDao {
 
-    @PersistenceContext
-    private EntityManager entityManager;
 
     @Override
     public Order findOrdersByPersonId(Long id) {
-        TypedQuery<Order> query = entityManager.createNamedQuery("Orders.findOrdersByPersonId", Order.class);
-        query.setParameter("id", id);
-        return query.getSingleResult();
+        return null;
     }
 
     @Override
     public Order save(Order order) {
-        if (order.getId() == null) {
-            entityManager.persist(order);
-        } else {
-            entityManager.merge(order);
-        }
-        return order;
+        return null;
     }
 }
